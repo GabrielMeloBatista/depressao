7b3adec3c582a0677d425cc3c23dd019c962dc70
Gabriel Melo Batista <64172710+GabrielMeloBatista@users.noreply.github.com>
2022-08-26 07:57:51 -0300

    tristesa agora com mdc

--- tristesa.c
+++ tristesa.c
@@ -2,76 +2,96 @@
 #include <stdlib.h>
 #include <math.h>
 
-void soma(int N1, int N2, int D1, int D2){
-	int Num, Den;
-	//(N1*D2 + N2*D1) / (D1*D2)
-	Num = N1*D2 + N2*D1;
-	Den = D1*D2;
+int maximoDivisorComum(int x, int y)
+{
+  if (y == 0)
+  {
+    return y;
+  }
+  else
+  {
+    return maximoDivisorComum(y, x % y);
+  }
 }
 
-void subtracao(int N1, int N2, int D1, int D2){
-	int Num, Den;
-	//(N1*D2 - N2*D1) / (D1*D2)
-	Num = N1*D2 - N2*D1;
-	Den = D1*D2;
+void soma(int N1, int N2, int D1, int D2, int *num, int *den)
+{
+  //(N1*D2 + N2*D1) / (D1*D2)
+  *num = N1 * D2 + N2 * D1;
+  *den = D1 * D2;
 }
 
-void multiplicacao(int N1, int N2, int D1, int D2){
-	int Num, Den;
-	//(N1*N2) / (D1*D2)
-	Num = N1 * N2;
-	Den = D1*D2;
+void subtracao(int N1, int N2, int D1, int D2, int *num, int *den)
+{
+  //(N1*D2 - N2*D1) / (D1*D2)
+  *num = N1 * D2 - N2 * D1;
+  *den = D1 * D2;
 }
 
-void divisao(int N1, int N2, int D1, int D2){
-	int Num, Den;
-	//(N1*D2)/(N2*D1)
-	Num = N1*D2;
-	Den = N2*D1;
+void multiplicacao(int N1, int N2, int D1, int D2, int *num, int *den)
+{
+  //(N1*N2) / (D1*D2)
+  *num = N1 * N2;
+  *den = D1 * D2;
 }
 
-void menu(int N1, int N2, int D1, int D2, char operador){
-	switch(operador){
-		case '+':
-			soma( N1,  N2,  D1,  D2);
-			break;
-			
-		case '-':
-			subtracao( N1,  N2,  D1,  D2);
-			break;
-			
-		case '*':
-			multiplicacao( N1,  N2,  D1,  D2);
-			break;
-			
-		case '/':
-			divisao( N1,  N2,  D1,  D2);
-			break;
-	}
+void divisao(int N1, int N2, int D1, int D2, int *num, int *den)
+{
+  //(N1*D2)/(N2*D1)
+  *num = N1 * D2;
+  *den = N2 * D1;
 }
 
-int main(){
-    int N, N1, N2, D1, D2;
-	int i = 0;
-	char operador;
-	scanf("%d", &N);
-	if(N >= 1 && N<= 10000){
-	    while(i < N){
-	    	scanf("%d", &N1);
-	    	scanf("%d", &D1);
-	    	scanf("%s", &operador);
-	    	getchar();
-	    	scanf("%d", &N2);
-	    	scanf("%d", &D2);
-	    	if(N1>= 1 && N1 <= 1000 && N2>= 1 && N2 <= 1000 && D1>= 1 && D1 <= 1000 && D2 >= 1 && D2 <= 1000){
-	    		menu(N1, N2, D1, D2, operador);
-			}
-			else{
-				break;
-			}
-	    	i++;
-		}
-	}
-    
-    return 0;
+void menu(int N1, int N2, int D1, int D2, char operador, int *num, int *den)
+{
+  switch (operador)
+  {
+  case '+':
+    soma(N1, N2, D1, D2, num, den);
+    break;
+
+  case '-':
+    subtracao(N1, N2, D1, D2, num, den);
+    break;
+
+  case '*':
+    multiplicacao(N1, N2, D1, D2, num, den);
+    break;
+
+  case '/':
+    divisao(N1, N2, D1, D2, num, den);
+    break;
+  }
+}
+
+int main()
+{
+  int N, N1, N2, D1, D2, menor, num, den;
+  int i = 0;
+  char operador;
+  scanf("%d", &N);
+  if (N >= 1 && N <= 10000)
+  {
+    while (i < N)
+    {
+      scanf("%d / %d %s", &N1, &D1, &operador);
+      getchar();
+      scanf(" %d / %d", &N2, &D2);
+      getchar();
+
+      if (N1 >= 1 && N1 <= 1000 && N2 >= 1 && N2 <= 1000 && D1 >= 1 && D1 <= 1000 && D2 >= 1 && D2 <= 1000)
+      {
+        menu(N1, N2, D1, D2, operador, &num, &den);
+        menor = maximoDivisorComum((num < 0 ? -num : num), den);
+        printf("%d / %d = %d / %d\n", num, den, num / menor, (!num ? 1 : den / menor));
+      }
+      else
+      {
+        break;
+      }
+      i++;
+    }
+  }
+
+  return 0;
 }
